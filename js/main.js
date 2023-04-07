ccbd14fc301c138583c9d22f6cc49d00ec1a33ec
axelruffini <118085062+axelruffini@users.noreply.github.com>
2023-04-07 17:18:54 +0000

    Funciones agregadas y toLowerCase

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 let cantTareas = prompt("Cuantas tareas desea registrar?");
 let tareas = [];
 let finalizadas = 0;
-let SinFinalizar = 0;
+let sinFinalizar = 0;
 
 function agregarTarea() {
     tareas.push(prompt("Ingrese la tarea: "));
 }
 
 function preguntarFinalizada(tarea) {
-    let tareaFinalizada = prompt("Finalizo la tarea " + tarea + " ? Ingrese Si o No: ");
+    let tareaFinalizada = prompt("Finalizo la tarea " + tarea + " ? Ingrese Si o No: ").toLowerCase();
     if (tareaFinalizada === "si") {
         finalizadas++;
     } else {
-        SinFinalizar++;
+        sinFinalizar++;
     }
 }
 
@@ -26,6 +26,6 @@ for (let contadorTareas2 = 0; contadorTareas2 < tareas.length; contadorTareas2++
 
 let porcentajeFinalizadas = (finalizadas / cantTareas) * 100;
 alert("Cantidad de tareas registradas: " + cantTareas);
-alert("Cantidad de tareas sin finalizar: " + SinFinalizar);
+alert("Cantidad de tareas sin finalizar: " + sinFinalizar);
 alert("Cantidad de tareas finalizadas: " + finalizadas);
 alert("Porcentaje de tareas finalizadas: " + porcentajeFinalizadas.toFixed(2) + "%");
