286338ee96751a7ba24308d2f63a4c684098dcd6
axelruffini <axel0064@gmail.com>
2023-05-03 16:59:30 -0300

    interfaz grafica

--- js/main.js
+++ js/main.js
@@ -1,67 +1,72 @@
-let tareas = [];
-let finalizadas = 0;
-let SinFinalizar = 0;
+// Obtener referencias a los elementos del DOM
+const formulario = document.querySelector('form');
+const nombreTareaInput = document.querySelector('#nombre-tarea');
+const descTareaInput = document.querySelector('#desc-tarea');
+const contenedorTareas = document.querySelector('#contenedor-tareas');
 
-///////////////////////////////////////////////  Ingreso de datos   /////////////////////////////////////////////////////////
+// Maneja el evento de envío del formulario
+formulario.addEventListener('submit', function (evento) {
+  // Prevenir la recarga de la página
+  evento.preventDefault();
 
-let cantTareas = prompt("Cuantas tareas desea registrar?"); //Cuadro de texto cantidad de tareas
+  // Obtener el valor del campo de nombre de tarea y descripción
+  const nombreTareaTexto = nombreTareaInput.value.trim();
+  const descTareaTexto = descTareaInput.value.trim();
 
-function agregarTarea() { //Objeto para el almacenamiento de tareas
-  let tareaNombre = prompt("Ingrese el nombre de la tarea: ");
-  let tareaDescripcion = prompt("Ingrese la descripción de la tarea: ");
-  tareas.push({nombre: tareaNombre, descripcion: tareaDescripcion});
+  // Crear un nuevo elemento de div para la tarea y agregarlo al contenedor
+  if (nombreTareaTexto && descTareaTexto) {
+    const tareaDiv = document.createElement('div');
+    const nombreTareaNode = document.createElement('h3');
+    const descTareaNode = document.createElement('p');
+    nombreTareaNode.textContent = nombreTareaTexto;
+    descTareaNode.textContent = descTareaTexto;
+    tareaDiv.appendChild(nombreTareaNode);
+    tareaDiv.appendChild(descTareaNode);
+    contenedorTareas.appendChild(tareaDiv);
+
+    // Limpiar los campos de nombre de tarea y descripción
+    nombreTareaInput.value = '';
+    descTareaInput.value = '';
+
+    // Guardar la tarea en el almacenamiento local
+    const tareas = obtenerTareasDelStorage();
+    tareas.push({ nombre: nombreTareaTexto, descripcion: descTareaTexto });
+    guardarTareasEnStorage(tareas);
+  }
+});
+
+// Obtiene las tareas almacenadas en el almacenamiento local
+function obtenerTareasDelStorage() {
+  let tareas = localStorage.getItem('tareas');
+  if (!tareas) {
+    tareas = [];
+  } else {
+    tareas = JSON.parse(tareas);
+  }
+  return tareas;
 }
 
-function preguntarFinalizada(tarea) {
-  let tareaFinalizada;
-  do {
-    tareaFinalizada = prompt("¿Finalizó la tarea " + tarea.nombre + "? Ingrese Si o No: ").toUpperCase();
-  } while (tareaFinalizada !== "SI" && tareaFinalizada !== "NO");
-  
-  if (tareaFinalizada === "SI") {
-    finalizadas++;
-  } else {
-    SinFinalizar++;
+// Guardar las tareas en el almacenamiento local
+function guardarTareasEnStorage(tareas) {
+  localStorage.setItem('tareas', JSON.stringify(tareas));
+}
+
+// Cargar las tareas almacenadas en el almacenamiento local y mostrarlas en la página
+function cargarTareas() {
+  const tareas = obtenerTareasDelStorage();
+  for (const tarea of tareas) {
+    const tareaDiv = document.createElement('div');
+    const nombreTareaNode = document.createElement('h3');
+    const descTareaNode = document.createElement('p');
+    nombreTareaNode.textContent = tarea.nombre;
+    descTareaNode.textContent = tarea.descripcion;
+    tareaDiv.appendChild(nombreTareaNodo);
+    tareaDiv.appendChild(descTareaNodo);
+    contenedorTareas.appendChild(tareaDiv);
   }
 }
 
-
-///////////////////////////////////////////////  Procesamiento de los datos   /////////////////////////////////////////////////////////
-for (let contadorTareas = 0; contadorTareas < cantTareas; contadorTareas++) { //Cuadro de texto para ingresar las tareas
-  agregarTarea();
-}
-
-for (let contadorTareas2 = 0; contadorTareas2 < tareas.length; contadorTareas2++) { //Cuadro de texto para ingresar las tareas
-  preguntarFinalizada(tareas[contadorTareas2]);
-}
-
-let porcentajeFinalizadas = (finalizadas / cantTareas) * 100;
-
-
-
-let listaTareas = "Tareas registradas: \n";
-
-for (let i = 0; i < tareas.length; i++) { //Crea el listado
-  listaTareas += (i + 1) + ". " + tareas[i].nombre + " - " + tareas[i].descripcion + "\n";
-}
-
-let buscarTarea = prompt("¿Qué tarea desea buscar?");
-
-let tareaBuscada = tareas.find(
-  (unaTarea) => unaTarea.nombre.toUpperCase() === buscarTarea.toUpperCase()
-);
-
-if (tareaBuscada) {
-  alert("La tarea: " + tareaBuscada.nombre + " fue encontrada, la decripcion es: " + tareaBuscada.descripcion);
-} else {
-  alert("La tarea no fue encontrada");
-}
-
-
-////////////////////////////////////////  Muestra de datos  ////////////////////////////////////////////////////////////////
-
-alert("Cantidad de tareas registradas: " + cantTareas); //Contador de tareas totales
-alert(listaTareas); //Muestra un listado de tareas
-alert("Cantidad de tareas finalizadas: " + finalizadas); //Muestra las tareas Finalizadas
-alert("Cantidad de tareas sin finalizar: " + SinFinalizar); //Muestra las tareas sin finalizar
-alert("Porcentaje de tareas finalizadas: " + porcentajeFinalizadas.toFixed(2) + "%"); //Muestra el porcentaje de tareas finalizadas
+// Cargar las tareas al cargar la página
+window.addEventListener('cargar', function () {
+  cargarTareas();
+});
