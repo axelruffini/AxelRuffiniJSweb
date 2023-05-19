39258075dae087b48e775dbc488bb8ac92549ac6
axelruffini <axel0064@gmail.com>
2023-05-18 21:59:45 -0300

    Nuevas funcionalidades

--- js/main.js
+++ js/main.js
@@ -8,25 +8,40 @@ const contenedorTareas = document.querySelector('#contenedor-tareas');
 function agregarFuncionalidadEliminarTarea(tareaDiv, tareas, index) {
   const botonEliminar = document.createElement('button');
   botonEliminar.textContent = 'Eliminar';
-  botonEliminar.addEventListener('click', function () {
-    tareaDiv.remove();
-    tareas.splice(index, 1);
-    guardarTareasEnStorage(tareas);
+  botonEliminar.classList.add('btn-eliminar');
+  botonEliminar.addEventListener('click', async () => {
+    const result = await Swal.fire({
+      icon: 'warning',
+      title: '¿Estás seguro?',
+      text: 'Esta acción eliminará la tarea',
+      showCancelButton: true,
+      confirmButtonText: 'Sí, eliminar',
+      cancelButtonText: 'Cancelar'
+    });
+    if (result.isConfirmed) {
+      tareaDiv.remove();
+      tareas.splice(index, 1);
+      await guardarTareasEnStorage(tareas);
+      Swal.fire(
+        '¡Eliminada!',
+        'La tarea ha sido eliminada.',
+        'success'
+      );
+    }
   });
   tareaDiv.appendChild(botonEliminar);
 }
 
 // Maneja el evento de envío del formulario
-formulario.addEventListener('submit', function (evento) {
-  // Prevenir la recarga de la página
-  evento.preventDefault();
+formulario.addEventListener('submit', async (evento) => {
+  evento.preventDefault(); // Prevenir la recarga de la página
 
   // Obtener el valor del campo de nombre de tarea y descripción
   const nombreTareaTexto = nombreTareaInput.value.trim();
   const descTareaTexto = descTareaInput.value.trim();
 
-  // Crear un nuevo elemento de div para la tarea y agregarlo al contenedor
   if (nombreTareaTexto && descTareaTexto) {
+    // Crear un nuevo elemento de div para la tarea y agregarlo al contenedor
     const tareaDiv = document.createElement('div');
     const nombreTareaNode = document.createElement('h3');
     const descTareaNode = document.createElement('p');
@@ -37,47 +52,61 @@ formulario.addEventListener('submit', function (evento) {
     contenedorTareas.appendChild(tareaDiv);
 
     // Agregar la funcionalidad de eliminar tarea
-    const tareas = obtenerTareasDelStorage();
-    const index = tareas.length;
-    agregarFuncionalidadEliminarTarea(tareaDiv, tareas, index);
-
-    // Limpiar los campos de nombre de tarea y descripción
-    nombreTareaInput.value = '';
-    descTareaInput.value = '';
-
-    // Guardar la tarea en el almacenamiento local
-    tareas.push({ nombre: nombreTareaTexto, descripcion: descTareaTexto });
-    guardarTareasEnStorage(tareas);
+    try {
+      const tareas = await obtenerTareasDelStorage();
+      const index = tareas.length;
+      agregarFuncionalidadEliminarTarea(tareaDiv, tareas, index);
+      await guardarTareasEnStorage(tareas.concat({ nombre: nombreTareaTexto, descripcion: descTareaTexto }));
+      
+      // Limpiar los campos de nombre de tarea y descripción
+      nombreTareaInput.value = '';
+      descTareaInput.value = '';
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
 });
 
 // Obtiene las tareas almacenadas en el almacenamiento local
 function obtenerTareasDelStorage() {
-  const tareas = JSON.parse(localStorage.getItem('tareas') || '[]');
-  return tareas;
+  return new Promise((resolve, reject) => {
+    const tareas = JSON.parse(localStorage.getItem('tareas') || '[]');
+    resolve(tareas);
+  });
 }
 
 // Guardar las tareas en el almacenamiento local
 function guardarTareasEnStorage(tareas) {
-  localStorage.setItem('tareas', JSON.stringify(tareas));
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      localStorage.setItem('tareas', JSON.stringify(tareas));
+      resolve();
+    }, 0);
+  });
 }
 
 // Cargar las tareas almacenadas en el almacenamiento local y mostrarlas en la página
-function cargarTareas() {
-  const tareas = obtenerTareasDelStorage();
-  for (const tarea of tareas) {
-    const tareaDiv = document.createElement('div');
-    const nombreTareaNode = document.createElement('h3');
-    const descTareaNode = document.createElement('p');
-    nombreTareaNode.textContent = tarea.nombre;
-    descTareaNode.textContent = tarea.descripcion;
-    tareaDiv.appendChild(nombreTareaNode);
-    tareaDiv.appendChild(descTareaNode);
-    contenedorTareas.appendChild(tareaDiv);
+async function cargarTareas() {
+  try {
+    const tareas = await obtenerTareasDelStorage();
+    for (const tarea of tareas) {
+      const tareaDiv = document.createElement('div');
+      const nombreTareaNode = document.createElement('h3');
+      const descTareaNode = document.createElement('p');
+      nombreTareaNode.textContent = tarea.nombre;
+      descTareaNode.textContent = tarea.descripcion;
+      tareaDiv.appendChild(nombreTareaNode);
+      tareaDiv.appendChild(descTareaNode);
+      contenedorTareas.appendChild(tareaDiv);
+
+      // Agregar la funcionalidad de eliminar tarea
+      const index = tareas.indexOf(tarea);
+      agregarFuncionalidadEliminarTarea(tareaDiv, tareas, index);
+    }
+  } catch (error) {
+    console.error('Error:', error);
   }
 }
 
 // Cargar las tareas al cargar la página
-window.addEventListener('carga', function () {
-  cargarTareas();
-});
+window.addEventListener('load', cargarTareas);
